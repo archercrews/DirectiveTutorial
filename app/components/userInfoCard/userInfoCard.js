56afd039b8e49b9dde8185d8afbfe449974f40fa
archercrews <crewsha@gmail.com>
2016-11-28 16:54:37 -0500

    Latest changes
    "Events, Scope and Controllers - Isolated Scopes - Function Parameters

--- app/components/userInfoCard/userInfoCard.js
+++ app/components/userInfoCard/userInfoCard.js
@@ -2,13 +2,49 @@
 'use strict';
 
 angular
+  .module('myApp');
+
+angular.module('myApp').controller('mainCtrl', ['$scope', function($scope) {
+  $scope.user1 = {
+    name: 'Luke Skywalker',
+    address: {
+      street: 'PO Box 123',
+      city: 'Secret Rebel Base',
+      planet: 'Yavin 4'
+    },
+    friends: [
+      'Han',
+      'Leia',
+      'Chewy'
+    ]
+  };
+  $scope.user2 = {
+    name: 'Han Solo',
+    address: {
+      street: 'PO Box 123',
+      city: 'Mos Eisley',
+      planet: 'Tattoine'
+    },
+    friends: [
+      'Luke',
+      'Leia',
+      'Chewy'
+    ]
+  };
+}]);
+
+angular
   .module('myApp')
   .directive('userInfoCard', userInfoCard);
 
   function userInfoCard(){
     var directive = {
       templateUrl: 'app/components/userInfoCard/userInfoCard.html',
-      controller: Controller
+      controller: Controller,
+      scope: {
+        initialCollapsed: '@collapsed',
+        user: '='
+      }
     };
 
     return directive;
@@ -17,18 +53,68 @@ angular
   Controller.$inject = ['$scope'];
 
   function Controller($scope) {
-    $scope.user = {
-      name: 'Luke Skywalker',
-      address: {
-        street: 'PO Box 123',
-        city: 'Secret Rebel Base',
-        planet: 'Yavin 4'
-      },
-      friends: [
-        'Han',
-        'Leia',
-        'Chewy'
-      ]
+
+    $scope.collapsedState = ($scope.initialCollapsed === 'true');
+    //$scope.collapsed = false;
+    $scope.collapse = function() {
+      $scope.collapsed = !$scope.collapsed;
+    }
+
+    $scope.knightMe = function(user) {
+      user.rank = "knight";
+    }
+  };
+
+angular
+  .module('myApp')
+  .directive('address', address);
+
+  function address(){
+    return {
+      restrict: 'E',
+      templateUrl: 'app/components/userInfoCard/address.html',
+      controller: addressController,
+      scope: true
+    }
+  }
+
+  addressController.$inject = ['$scope'];
+  function addressController($scope) {
+    $scope.collapsed = false;
+    $scope.collapseAddress = function() {
+      $scope.collapsed = true;
+    }
+    $scope.expandAddress = function() {
+      $scope.collapsed = false;
+    }
+  };
+
+angular
+  .module('myApp')
+  .directive('removeFriend', removeFriend);
+
+function removeFriend() {
+    return {
+      restrict: 'E',
+      templateUrl: 'app/components/userinfoCard/removeFriend.html',
+      controller: 'removeController'
     };
   };
+
+  Controller.$inject = ['$scope'];
+  function removeController($scope) {
+    $scope.removing = false;
+    $scope.startRemove = function() {
+      $scope.removing = true;
+    };
+    $scope.cancelRemove = function() {
+      $scope.removing = false;
+    };
+    $scope.removeFriend = function(friend) {
+      var idx = $scope.user.friends.indexOf(friend);
+      if(idx > -1) {
+        $scope.user.friends.splice(idx , 1);
+      }
+    };
+   };
 })();
